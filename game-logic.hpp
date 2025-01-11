be34e119539bdcc2b28d9350c3d29ab7a661a129
alon <alonnoga314@gmail.com>
2025-01-11 22:25:16 +0200

    added the code

---
+++ game-logic.hpp
@@ -0,0 +1,48 @@
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+#include <string>  
+#include <vector>
+#include <variant>
+#include <utility> // for std::pair
+#include <optional>
+#include <functional>
+
+
+namespace GameLogic {
+	constexpr std::string_view X = "X";
+	constexpr std::string_view O = "O";
+	constexpr std::string_view EMPTY = " ";
+
+	using Cell = std::string_view; // one of X, Y, EMPTY
+	using Player = std::string_view; // one of X, Y
+	using Board = std::vector<std::vector<std::string_view>>;
+
+	struct PutEvent {
+		int x;
+		int y;
+		Player piece;
+	};
+
+	struct Position {
+		int x;
+		int y;
+	};
+
+	struct WinEvent {
+		Player winner;
+		std::vector<Position> sequence;
+	};
+
+	struct TurnChangedEvent {
+		Player player;
+	};
+
+	using Event = std::variant<PutEvent, WinEvent, TurnChangedEvent>;
+
+	void onChange(int x, int y, std::function<void(Event)> onEvent);
+	const Board& getBoard();
+	const Player& getActivePlayer();
+}
+
+#endif
---
+++ tic-tac-toe.cpp
@@ -0,0 +1,12 @@
+#include "tic-tac-toe.h"
+#include "game-logic.hpp"
+#include "game-ui.hpp"
+
+int main() {
+	GameUI::init(GameLogic::getBoard(), GameLogic::getActivePlayer());
+	GameUI::render([](int x, int y) {
+		GameLogic::onChange(x, y, GameUI::handleGameEvent);
+		});
+
+	return EXIT_SUCCESS;
+}
